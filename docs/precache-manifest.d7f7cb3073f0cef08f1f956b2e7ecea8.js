eb5cacf5774bc74266f34dc601069686553df195
Daniel Hernández <danielht97-@outlook.com>
2020-07-27 13:00:28 -0500

    docs agregado

---
+++ docs/precache-manifest.d7f7cb3073f0cef08f1f956b2e7ecea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cd7672a43c7839ddd8ad4950431fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ded5d3a4b3eaa22695",
+    "url": "/static/css/main.0619c05c.chunk.css"
+  },
+  {
+    "revision": "eb00bc0e5d1d87ed4120",
+    "url": "/static/js/2.34972069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34972069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ded5d3a4b3eaa22695",
+    "url": "/static/js/main.d22cbefc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
